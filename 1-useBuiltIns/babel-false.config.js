1afa297ba6dbc8981c492ea597088dcea1300be3
Chunrong Chen <chunrong.chen@seamoney.com>
2022-05-11 16:42:10 +0800

    feat: update folder name

--- 1-useBuiltIns/babel-false.config.js
+++ 1-useBuiltIns/babel-false.config.js
@@ -1,5 +1,12 @@
 module.exports = (api) => {
-  // 缓存配置
+  // 1.output babel version which reads the config file
+  console.log("api.version", api.version);
+
+  api.assertVersion("^7.1");
+
+  console.log("api.assertVersion", api.version);
+
+  // 2.
   api.cache(true);
 
   return {
